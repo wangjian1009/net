1f9bf85931d088fc7524a081a8169683bdd8e568
Lokiwang <wangjian1009@gmail.com>
2018-06-30 12:31:59 +0800

    update endpoint

--- core/include/net_endpoint.h
+++ core/include/net_endpoint.h
@@ -12,6 +12,7 @@ void net_endpoint_free(net_endpoint_t endpoint);
 net_schedule_t net_endpoint_schedule(net_endpoint_t endpoint);
 net_endpoint_type_t net_endpoint_type(net_endpoint_t endpoint);
 net_protocol_t net_endpoint_protocol(net_endpoint_t endpoint);
+int net_endpoint_set_protocol(net_endpoint_t endpoint, net_protocol_t protocol);
 net_driver_t net_endpoint_driver(net_endpoint_t endpoint);
 
 uint32_t net_endpoint_id(net_endpoint_t endpoint);
