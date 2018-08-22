e475df0ee7187a520c386a096fea6372af90b9e4
Lokiwang <wangjian1009@gmail.com>
2018-08-23 07:43:56 +0800

    net_dq_endpoint调度on_read错误导致endpoint释放的bug解决

--- driver_dq/src/net_dq_endpoint.m
+++ driver_dq/src/net_dq_endpoint.m
@@ -314,15 +314,11 @@ int net_dq_endpoint_set_established(net_dq_driver_t driver, net_dq_endpoint_t en
 }
 
 int net_dq_endpoint_on_read(net_dq_driver_t driver, net_dq_endpoint_t endpoint, net_endpoint_t base_endpoint) {
-    while(1) {
-        if (net_endpoint_state(base_endpoint) != net_endpoint_state_established) {
-            CPE_ERROR(
-                driver->m_em, "dq: %s: on read: state is %s, can`t read",
-                net_endpoint_dump(net_dq_driver_tmp_buffer(driver), base_endpoint),
-                net_endpoint_state_str(net_endpoint_state(base_endpoint)));
-            return -1;
-        }
-
+    while(
+        endpoint->m_source_r == NULL /*can read*/
+        && net_endpoint_state(base_endpoint) == net_endpoint_state_established
+        )
+    {
         uint32_t capacity = 0;
         void * rbuf = net_endpoint_buf_alloc(base_endpoint, net_ep_buf_read, &capacity);
         if (rbuf == NULL) {
@@ -392,6 +388,8 @@ int net_dq_endpoint_on_read(net_dq_driver_t driver, net_dq_endpoint_t endpoint, 
         }
         assert(0);
     }
+
+    return 0;
 }
 
 int net_dq_endpoint_on_write(net_dq_driver_t driver, net_dq_endpoint_t endpoint, net_endpoint_t base_endpoint) {
