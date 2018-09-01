bd1d4ab18b5538fad495734a34379ebcc0967ad3
Lokiwang <wangjian1009@gmail.com>
2018-09-01 14:30:55 +0800

    net_address 填写sock结构的len

--- core/src/net_address.c
+++ core/src/net_address.c
@@ -274,6 +274,7 @@ TRY_AGAIN:
 
         bzero(s, sizeof(*s));
         s->sin_family = AF_INET;
+        s->sin_len = sizeof(*s);
         s->sin_port = htons(address_ipv4v6->m_port);
         s->sin_addr.s_addr = address_ipv4v6->m_ipv4.u32;
         *addr_len = sizeof(*s);
@@ -293,7 +294,9 @@ TRY_AGAIN:
         
         bzero(s, sizeof(*s));
         s->sin6_family = AF_INET6;
+        s->sin6_len = sizeof(*s);
         s->sin6_port = htons(address_ipv4v6->m_port);
+        assert(sizeof(s->sin6_addr) == sizeof(address_ipv4v6->m_ipv6));
         memcpy(&s->sin6_addr, &address_ipv4v6->m_ipv6, sizeof(address_ipv4v6->m_ipv6));
         *addr_len = sizeof(*s);
         
