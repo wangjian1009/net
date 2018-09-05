ece1a842a657b92d710f0f4f76877ab5f07a911e
Lokiwang <wangjian1009@gmail.com>
2018-09-05 20:49:59 +0800

    移除不用的openssl,curl,c-ares库

--- depends/openssl/android/lib/Android-crypto.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := openssl-crypto
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libcrypto.a
-include $(PREBUILT_STATIC_LIBRARY)
--- depends/openssl/android/lib/Android-ssl.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := openssl-ssl
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libssl.a
-include $(PREBUILT_STATIC_LIBRARY)
-
