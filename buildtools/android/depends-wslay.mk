cfa05281e4c33baeea0f45a029e5fc877c65e327
Lokiwang <wangjian1009@gmail.com>
2018-10-12 16:22:27 +0800

    android build srcipt update

--- buildtools/android/depends-wslay.mk
+++ buildtools/android/depends-wslay.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := wslay
-LOCAL_CFLAGS += $(if $(filter 0,$(APKD)),,-DDEBUG=1)
+LOCAL_CFLAGS += $(if $(filter 0,$(APKD)),,-DDEBUG=1) -DHAVE_NETINET_IN_H
 LOCAL_C_INCLUDES +=  $(LOCAL_PATH)/../../depends/wslay/include
 LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/../../depends/wslay/src/*.c)
 
