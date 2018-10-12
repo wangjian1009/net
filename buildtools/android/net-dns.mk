ec931c42a1ed51ac91463353722ebad6357da3ec
Lokiwang <wangjian1009@gmail.com>
2018-10-12 10:50:57 +0800

    补充android编译脚本

--- buildtools/android/net-dns.mk
+++ buildtools/android/net-dns.mk
@@ -2,13 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := net_protocol_socks5
+LOCAL_MODULE := net_dns
 LOCAL_EXPORT_CFLAGS += $(if $(filter 0,$(APKD)),,-g)
 LOCAL_CFLAGS += $(if $(filter 0,$(APKD)),,-DDEBUG=1) -Wall
 LOCAL_C_INCLUDES +=  $(LOCAL_PATH)/../../../cpe/include \
                      $(LOCAL_PATH)/../../core/include \
-                     $(LOCAL_PATH)/../../protocol_socks5/include
+                     $(LOCAL_PATH)/../../dns/include
 LOCAL_LDLIBS := 
-LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/../../protocol_socks5/src/*.c)
+LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/../../dns/src/*.c)
 
 include $(BUILD_STATIC_LIBRARY)
