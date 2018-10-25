ed7db20556b15d2830c92f6a5be49afe839d1fc8
qiangbin <qiangbin@adups.com>
2018-10-25 16:44:46 +0800

    android build bug fix

--- buildtools/android/net-protocol-proxy-http.mk
+++ buildtools/android/net-protocol-proxy-http.mk
@@ -9,6 +9,6 @@ LOCAL_C_INCLUDES +=  $(LOCAL_PATH)/../../../cpe/include \
                      $(LOCAL_PATH)/../../core/include \
                      $(LOCAL_PATH)/../../protocol_proxy_http/include
 LOCAL_LDLIBS := 
-LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/../../protocol_proxy_http/src/*.c)
+LOCAL_SRC_FILES += $(patsubst $(LOCAL_PATH)/%,%,$(wildcard $(LOCAL_PATH)/../../protocol_proxy_http/src/*.c))
 
 include $(BUILD_STATIC_LIBRARY)
--- buildtools/android/net-protocol-socks5.mk
+++ buildtools/android/net-protocol-socks5.mk
@@ -9,6 +9,6 @@ LOCAL_C_INCLUDES +=  $(LOCAL_PATH)/../../../cpe/include \
                      $(LOCAL_PATH)/../../core/include \
                      $(LOCAL_PATH)/../../protocol_socks5/include
 LOCAL_LDLIBS := 
-LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/../../protocol_socks5/src/*.c)
+LOCAL_SRC_FILES += $(patsubst $(LOCAL_PATH)/%,%,$(wildcard $(LOCAL_PATH)/../../protocol_socks5/src/*.c))
 
 include $(BUILD_STATIC_LIBRARY)
