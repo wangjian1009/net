7c3556043bb6061a898bcb1fd0115d677588fb70
Lokiwang <wangjian1009@gmail.com>
2018-11-16 18:09:04 +0800

    http_endpoint在调用disable以后不再自动重连

--- protocol_http/src/net_http_endpoint.c
+++ protocol_http/src/net_http_endpoint.c
@@ -211,7 +211,9 @@ void net_http_endpoint_enable(net_http_endpoint_t http_ep) {
 }
 
 int net_http_endpoint_disable(net_http_endpoint_t http_ep) {
-    return net_endpoint_set_state(http_ep->m_endpoint,  net_endpoint_state_disable);
+    if (net_endpoint_set_state(http_ep->m_endpoint, net_endpoint_state_disable) != 0) return -1;
+    net_timer_cancel(http_ep->m_connect_timer);
+    return 0;
 }
 
 int net_http_endpoint_set_state(net_http_endpoint_t http_ep, net_http_state_t state) {
