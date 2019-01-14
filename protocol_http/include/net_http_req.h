884334f18e5a110fd0cd3b679da87181fe8fe4d4
Lokiwang <wangjian1009@gmail.com>
2019-01-14 16:51:27 +0800

    http_req增加取消接口

--- protocol_http/include/net_http_req.h
+++ protocol_http/include/net_http_req.h
@@ -14,8 +14,8 @@ net_http_req_t net_http_req_find(net_http_endpoint_t http_ep, uint16_t req_id);
 
 int net_http_req_start_timer(net_http_req_t req, uint32_t timeout_ms);
 
-void net_http_req_cancel(net_http_req_t req);
-void net_http_req_cancel_by_id(net_http_endpoint_t http_ep, uint16_t req_id);
+void net_http_req_cancel_and_free(net_http_req_t req);
+void net_http_req_cancel_and_free_by_id(net_http_endpoint_t http_ep, uint16_t req_id);
     
 /*write*/
 net_http_req_state_t net_http_req_state(net_http_req_t req);
