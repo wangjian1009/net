a3a678c475846959ad279619d4c4b89898f7904f
Lokiwang <wangjian1009@gmail.com>
2019-03-12 10:39:08 +0800

    remove net_driver_ne

--- driver_ne/src/net_ne_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef NET_NE_UTILS_H_INCLEDED
-#define NET_NE_UTILS_H_INCLEDED
-#import <NetworkExtension/NWHostEndpoint.h>
-#include "net_ne_driver_i.h"
-
-NWHostEndpoint * net_ne_address_to_endoint(net_ne_driver_t driver, net_address_t address);
-net_address_t net_ne_endpoint_to_address(net_ne_driver_t driver, NWEndpoint * endpoint);
-
-#endif
