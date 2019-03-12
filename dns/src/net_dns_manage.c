b5e619ecf937580c5372a919aae85f292cbb876f
Lokiwang <wangjian1009@gmail.com>
2019-03-12 16:08:01 +0800

    update for dns

--- dns/src/net_dns_manage.c
+++ dns/src/net_dns_manage.c
@@ -257,8 +257,8 @@ static int net_dns_manage_local_query(void * ctx, const char * hostname, net_add
 static void net_dns_manage_do_delay_process(net_timer_t timer, void * input_ctx) {
     net_dns_manage_t manage = input_ctx;
 
-    manage->m_delay_process = NULL;
-
+    CPE_ERROR(manage->m_em, "xxxxx: delay process");
+    
     while(!TAILQ_EMPTY(&manage->m_complete_tasks)) {
         net_dns_task_t task = TAILQ_FIRST(&manage->m_complete_tasks);
 
