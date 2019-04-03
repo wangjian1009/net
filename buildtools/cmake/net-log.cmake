a5a3ed5b936fa572c9a56911a8b817b1a3e632ce
Lokiwang <wangjian1009@gmail.com>
2019-04-03 14:04:55 +0800

    update for cmake

---
+++ buildtools/cmake/net-log.cmake
@@ -0,0 +1,16 @@
+set(net_log_base ${CMAKE_CURRENT_LIST_DIR}/../../log)
+
+file(GLOB net_log_source ${net_log_base}/src/*.c)
+
+add_library(net_log STATIC ${net_log_source})
+
+set_property(TARGET net_log PROPERTY INCLUDE_DIRECTORIES
+  ${CMAKE_CURRENT_LIST_DIR}/../../depends/curl/include
+  ${CMAKE_CURRENT_LIST_DIR}/../../depends/mbedtls/include/mbedtls
+  ${CMAKE_CURRENT_LIST_DIR}/../../depends/libev/include
+  ${CMAKE_CURRENT_LIST_DIR}/../../../cpe/include
+  ${CMAKE_CURRENT_LIST_DIR}/../../core/include
+  ${CMAKE_CURRENT_LIST_DIR}/../../driver_ev/include
+  ${CMAKE_CURRENT_LIST_DIR}/../../log/include
+  ${net_log_base}/include
+  )
