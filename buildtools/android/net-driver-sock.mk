7fea6f69944b1e5637d9dccbf0ad2936230b4a9e
Lokiwang <wangjian1009@gmail.com>
2019-05-18 17:07:14 +0800

    driver-sock增加android脚本

--- buildtools/android/net-driver-sock.mk
+++ buildtools/android/net-driver-sock.mk
@@ -2,15 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := net_driver_ev
+LOCAL_MODULE := net_driver_sock
 LOCAL_EXPORT_CFLAGS += $(if $(filter 0,$(APKD)),,-g)
 LOCAL_CFLAGS += $(if $(filter 0,$(APKD)),,-DDEBUG=1) -Wall -Wno-unused-value
 LOCAL_C_INCLUDES +=  $(LOCAL_PATH)/../../../cpe/include \
-                     $(LOCAL_PATH)/../../depends/libev/include \
                      $(LOCAL_PATH)/../../core/include \
-                     $(LOCAL_PATH)/../../driver_sock/include \
-                     $(LOCAL_PATH)/../../driver_ev/include
+                     $(LOCAL_PATH)/../../driver_sock/include
 LOCAL_LDLIBS := 
-LOCAL_SRC_FILES += $(patsubst $(LOCAL_PATH)/%,%,$(wildcard $(LOCAL_PATH)/../../driver_ev/src/*.c))
+LOCAL_SRC_FILES += $(patsubst $(LOCAL_PATH)/%,%,$(wildcard $(LOCAL_PATH)/../../driver_sock/src/*.c))
 
 include $(BUILD_STATIC_LIBRARY)
