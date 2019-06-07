5ee027a577310034bdf88b7062a0620147bb4453
Lokiwang <wangjian1009@gmail.com>
2019-06-07 09:27:34 +0800

    net_dns_entry增加次数循环保护

--- dns/src/net_dns_entry.c
+++ dns/src/net_dns_entry.c
@@ -129,8 +129,9 @@ net_dns_entry_select_item(net_dns_entry_t entry, net_dns_item_select_policy_t po
     struct net_dns_entry_item_it item_it;
     net_dns_entry_items(entry, &item_it, 1);
 
-    uint8_t protect = 0; 
-    for(check = net_dns_entry_item_it_next(&item_it); check && protect < 100; check = next, protect++) {
+    uint32_t protect = 0;
+    uint32_t protect_limit = 100;
+    for(check = net_dns_entry_item_it_next(&item_it); check && protect < protect_limit; check = next, protect++) {
         next = net_dns_entry_item_it_next(&item_it);
         assert(next != check);
 
@@ -157,6 +158,7 @@ net_dns_entry_select_item(net_dns_entry_t entry, net_dns_item_select_policy_t po
             break;
         }
     }
+    assert(protect < protect_limit);
 
     return item;
 }
