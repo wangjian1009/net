32f836b02b542179b9c294fd17c8da6c59234476
Lokiwang <wangjian1009@gmail.com>
2019-07-22 09:26:00 +0800

    cmake add net_driver_dq

---
+++ buildtools/cmake/net-driver-dq.cmake
@@ -0,0 +1,13 @@
+set(net_driver_dq_base ${CMAKE_CURRENT_LIST_DIR}/../../driver_dq)
+
+file(GLOB net_driver_dq_source ${net_driver_dq_base}/src/*.m)
+
+add_library(net_driver_dq STATIC ${net_driver_dq_source})
+
+set_property(TARGET net_driver_dq PROPERTY INCLUDE_DIRECTORIES
+  ${CMAKE_CURRENT_LIST_DIR}/../../../cpe/pal/include
+  ${CMAKE_CURRENT_LIST_DIR}/../../../cpe/utils/include
+  ${CMAKE_CURRENT_LIST_DIR}/../../core/include
+  ${CMAKE_CURRENT_LIST_DIR}/../../driver_sock/include
+  ${net_driver_dq_base}/include
+  )
