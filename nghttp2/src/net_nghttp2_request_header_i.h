d95564170be67c9d57a8edabac13632d00884fc9
Lokiwang <wangjian1009@gmail.com>
2019-11-25 19:10:15 +0800

    update

---
+++ nghttp2/src/net_nghttp2_request_header_i.h
@@ -0,0 +1,23 @@
+#ifndef NET_EBB_REQUEST_HEADER_H_I_INCLEDED
+#define NET_EBB_REQUEST_HEADER_H_I_INCLEDED
+#include "net_nghttp2_request_header.h"
+#include "net_nghttp2_request_i.h"
+
+struct net_nghttp2_request_header {
+    net_nghttp2_request_t m_request;
+    TAILQ_ENTRY(net_nghttp2_request_header) m_next;
+    uint16_t m_index;
+    char * m_name;
+    char * m_value;
+    char m_buf[128];
+};
+
+net_nghttp2_request_header_t net_nghttp2_request_header_create(net_nghttp2_request_t request, uint16_t index, const char * name, size_t name_len);
+void net_nghttp2_request_header_free(net_nghttp2_request_header_t header);
+void net_nghttp2_request_header_real_free(net_nghttp2_request_header_t header);
+
+net_nghttp2_request_header_t net_nghttp2_request_header_find_by_index(net_nghttp2_request_t request, uint16_t index);
+
+int net_nghttp2_request_header_set_value(net_nghttp2_request_header_t header, const char * value, size_t value_len);
+
+#endif
