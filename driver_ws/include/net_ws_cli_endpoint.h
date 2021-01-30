1792e881503aca5237522559074c7ebf3630e392
Lokiwang <wangjian1009@gmail.com>
2021-01-30 14:58:34 +0800

    补充ws_cli_endpoint握手代码

--- driver_ws/include/net_ws_cli_endpoint.h
+++ driver_ws/include/net_ws_cli_endpoint.h
@@ -7,8 +7,12 @@ NET_BEGIN_DECL
 net_ws_cli_endpoint_t net_ws_cli_endoint_cast(net_endpoint_t endpoint);
 net_endpoint_t net_ws_cli_endpoint_stream(net_endpoint_t endpoint);
 
-int net_ws_cli_endpoint_set_path(net_ws_cli_endpoint_t ws_endpoint);
-    
+const char * net_ws_cli_endpoint_path(net_ws_cli_endpoint_t endpoint);
+int net_ws_cli_endpoint_set_path(net_ws_cli_endpoint_t endpoint, const char * path);
+
+/* int net_ws_cli_endpoint_send_msg_text(net_ws_endpoint_t ws_ep, const char * msg); */
+/* int net_ws_cli_endpoint_send_msg_bin(net_ws_endpoint_t ws_ep, const void * msg, uint32_t msg_len); */
+
 NET_END_DECL
 
 #endif
