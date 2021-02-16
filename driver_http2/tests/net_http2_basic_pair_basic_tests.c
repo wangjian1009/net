7c238c8eb11a68f692faae90b2cc17760fab21be
Lokiwang <wangjian1009@gmail.com>
2021-02-16 12:57:53 +0800

    update

--- driver_http2/tests/net_http2_basic_pair_basic_tests.c
+++ driver_http2/tests/net_http2_basic_pair_basic_tests.c
@@ -72,7 +72,7 @@ static void net_http2_basic_pair_basic(void **state) {
 
     assert_string_equal(
         net_http2_req_state_str(net_http2_req_state(req)),
-        net_http2_req_state_str(net_http2_req_state_head_sended));
+        net_http2_req_state_str(net_http2_req_state_head));
     
     test_net_driver_run(env->m_tdriver, 0);
     
