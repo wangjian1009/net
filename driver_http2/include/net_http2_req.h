323ea7e3e7436cd64b1243e1d3f422a21e6cf75f
Lokiwang <wangjian1009@gmail.com>
2021-02-16 22:05:40 +0800

    update

--- driver_http2/include/net_http2_req.h
+++ driver_http2/include/net_http2_req.h
@@ -32,20 +32,20 @@ net_http2_stream_t net_http2_req_stream(net_http2_req_t req);
 net_http2_req_state_t net_http2_req_state(net_http2_req_t req);
 
 /*请求*/
-int net_http2_req_add_req_head(net_http2_req_t http_req, const char * attr_name, const char * attr_value);
+int net_http2_req_add_req_head(net_http2_req_t req, const char * attr_name, const char * attr_value);
 const char * net_http2_req_find_req_header(net_http2_req_t req, const char * name);
 
-int net_http2_req_start_request(net_http2_req_t http_req, uint8_t have_follow_data);
-int net_http2_req_start_response(net_http2_req_t http_req, uint8_t have_follow_data);
-int net_http2_req_append(net_http2_req_t http_req, void const * data, uint32_t data_len, uint8_t have_follow_data);
+int net_http2_req_start_request(net_http2_req_t req, uint8_t have_follow_data);
+int net_http2_req_start_response(net_http2_req_t req, uint8_t have_follow_data);
+int net_http2_req_append(net_http2_req_t req, void const * data, uint32_t data_len, uint8_t have_follow_data);
 
 /*请求处理 */
 typedef uint8_t (*net_http2_req_on_write_fun_t)(void * ctx, net_http2_req_t req, void * output, uint32_t * output_len);
 
-uint8_t net_http2_req_is_writing(net_http2_req_t http_req);
+uint8_t net_http2_req_is_writing(net_http2_req_t req);
 
 int net_http2_req_write(
-    net_http2_req_t http_req,
+    net_http2_req_t req,
     void * write_ctx, net_http2_req_on_write_fun_t on_write, void (*write_ctx_free)(void *),
     uint8_t have_follow_data);
 
@@ -62,7 +62,7 @@ int net_http2_req_set_reader(
 
 void net_http2_req_clear_reader(net_http2_req_t req);
 
-int net_http2_req_add_res_head(net_http2_req_t http_req, const char * attr_name, const char * attr_value);
+int net_http2_req_add_res_head(net_http2_req_t req, const char * attr_name, const char * attr_value);
 const char * net_http2_req_find_res_header(net_http2_req_t req, const char * name);
 
 const char *  net_http2_req_method_str(net_http2_req_method_t method);
