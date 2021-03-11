7aca6c7dd5f0fda9accbc041a59fb1df207bda1b
Lokiwang <wangjian1009@gmail.com>
2021-03-11 11:31:16 +0800

    update

--- buildtools/cmake/net-ebb.cmake
+++ buildtools/cmake/net-ebb.cmake
@@ -3,7 +3,11 @@ set(net_ebb_base ${CMAKE_CURRENT_LIST_DIR}/../../ebb)
 file(GLOB net_ebb_source ${net_ebb_base}/src/*.c)
 
 find_package(RAGEL 6.6 REQUIRED)
-RAGEL_TARGET(ebb_request_parser ${net_ebb_base}/src/net_ebb_request_parser.rl ${CMAKE_SOURCE_DIR}/net_ebb_request_parser.c)
+
+RAGEL_TARGET(
+  ebb_request_parser
+  ${net_ebb_base}/src/net_ebb_request_parser.rl
+  ${CMAKE_BINARY_DIR}/net_ebb_request_parser.c)
 set(net_ebb_source ${net_ebb_source} ${RAGEL_ebb_request_parser_OUTPUTS})
 
 add_library(net_ebb STATIC ${net_ebb_source})
