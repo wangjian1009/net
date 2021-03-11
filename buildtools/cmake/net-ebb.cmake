9657c0ab0015f5c32d9f75dec9e26ae657f307f6
Lokiwang <wangjian1009@gmail.com>
2021-03-11 10:53:02 +0800

    net-ebb增加ragle处理

--- buildtools/cmake/net-ebb.cmake
+++ buildtools/cmake/net-ebb.cmake
@@ -2,6 +2,12 @@ set(net_ebb_base ${CMAKE_CURRENT_LIST_DIR}/../../ebb)
 
 file(GLOB net_ebb_source ${net_ebb_base}/src/*.c)
 
+find_package(RAGEL 6.6 REQUIRED)
+
+if(RAGEL_EXECUTABLE)
+  RAGEL_TARGET(ebb_request_parser ${net_ebb_base}/src/net_ebb_request_parser.rl ${net_ebb_base}/src/net_ebb_request_parser.c)
+endif()
+
 add_library(net_ebb STATIC ${net_ebb_source})
 
 set_property(TARGET net_ebb PROPERTY INCLUDE_DIRECTORIES
