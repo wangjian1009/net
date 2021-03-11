8b3de14d803a844b31db0dbb35e3dc4e5eeb9398
Lokiwang <wangjian1009@gmail.com>
2021-03-11 10:57:46 +0800

    net_ebb通过ragel直接生成解析文件

--- buildtools/cmake/net-ebb.cmake
+++ buildtools/cmake/net-ebb.cmake
@@ -3,10 +3,8 @@ set(net_ebb_base ${CMAKE_CURRENT_LIST_DIR}/../../ebb)
 file(GLOB net_ebb_source ${net_ebb_base}/src/*.c)
 
 find_package(RAGEL 6.6 REQUIRED)
-
-if(RAGEL_EXECUTABLE)
-  RAGEL_TARGET(ebb_request_parser ${net_ebb_base}/src/net_ebb_request_parser.rl ${net_ebb_base}/src/net_ebb_request_parser.c)
-endif()
+RAGEL_TARGET(ebb_request_parser ${net_ebb_base}/src/net_ebb_request_parser.rl ${CMAKE_SOURCE_DIR}/net_ebb_request_parser.c)
+set(net_ebb_source ${net_ebb_source} ${RAGEL_ebb_request_parser_OUTPUTS})
 
 add_library(net_ebb STATIC ${net_ebb_source})
 
@@ -15,4 +13,5 @@ set_property(TARGET net_ebb PROPERTY INCLUDE_DIRECTORIES
   ${CMAKE_CURRENT_LIST_DIR}/../../../cpe/utils/include
   ${CMAKE_CURRENT_LIST_DIR}/../../core/include
   ${net_ebb_base}/include
+  ${net_ebb_base}/src
   )
