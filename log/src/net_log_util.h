023fa6441104ef1b0953b66899ed8d865ca869bd
Lokiwang <wangjian1009@gmail.com>
2021-03-18 08:49:00 +0800

    清理掉net-log

--- log/src/net_log_util.h
+++
@@ -1,8 +0,0 @@
-#ifndef NET_LOG_UTIL_I_H_INCLEDED
-#define NET_LOG_UTIL_I_H_INCLEDED
-#include "net_log_schedule_i.h"
-
-void md5_to_string(const char * buffer, int bufLen, char * md5);
-int signature_to_base64(const char * sig, int sigLen, const char * key, int keyLen, char * base64);
-
-#endif
