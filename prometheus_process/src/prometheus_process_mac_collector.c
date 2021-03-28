b0635d412464fec74d4ece000a2164d443b50464
Lokiwang <wangjian1009@gmail.com>
2021-03-28 22:11:16 +0800

    增加mac获取进程内存大小

--- prometheus_process/src/prometheus_process_mac_collector.c
+++ prometheus_process/src/prometheus_process_mac_collector.c
@@ -1,11 +1,11 @@
+#if CPE_OS_MAC
 #include <assert.h>
+#include <mach/mach.h>
 #include "prometheus_collector.h"
 #include "prometheus_collector_metric.h"
 #include "prometheus_metric.h"
 #include "prometheus_gauge.h"
 #include "prometheus_process_collector_i.h"
-
-#if CPE_OS_MAC
 
 void prometheus_process_collector_mac_collect(prometheus_collector_t base_collector) {
     prometheus_process_collector_t collector = prometheus_collector_data(base_collector);
@@ -15,12 +15,24 @@ void prometheus_process_collector_mac_collect(prometheus_collector_t base_collec
     prometheus_collector_metric_t virtual_memory_bytes =
         prometheus_collector_metric_find_by_metric(
             base_collector, prometheus_process_provider_virtual_memory_bytes(provider));
-    
+    if (virtual_memory_bytes) {
+        struct task_basic_info info;
+        mach_msg_type_number_t size = TASK_BASIC_INFO_COUNT; //sizeof(info);
+        kern_return_t kerr = task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&info, &size);
+        if (kerr != KERN_SUCCESS) {
+        }
 
-/* struct task_basic_info info; */
-/* mach_msg_type_number_t size = TASK_BASIC_INFO_COUNT;//sizeof(info); */
-/* kern_return_t kerr = task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&info, &size); */
-/* return (kerr == KERN_SUCCESS) ? info.resident_size : 0;  */
+        prometheus_metric_t metric = prometheus_collector_metric_metric(virtual_memory_bytes);
+        prometheus_gauge_t guage = prometheus_gauge_cast(metric);
+        if (prometheus_gauge_set(guage, info.resident_size, NULL) != 0) {
+            CPE_ERROR(
+                provider->m_em, "prometheus: process: collect: %s: set gauge failed",
+                prometheus_metric_name(metric));
+            return;
+        }
+
+        prometheus_collector_metric_set_state(virtual_memory_bytes, prometheus_metric_collected);
+    }
 }
 
 
