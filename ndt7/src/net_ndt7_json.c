5c7ed613359b0f9ac2d2e64b1750131e8b3c2cb7
Lokiwang <wangjian1009@gmail.com>
2021-05-06 13:00:50 +0800

    update

--- ndt7/src/net_ndt7_json.c
+++ ndt7/src/net_ndt7_json.c
@@ -1,3 +1,20 @@
 #include "net_ndt7_json_i.h"
 
+int net_ndt7_response_to_json(yajl_gen gen, net_ndt7_response_t response) {
+    return 0;
+}
+
+int net_ndt7_response_from_json(net_ndt7_response_t response, yajl_val val, error_monitor_t em) {
+    return 0;
+}
+
+int net_ndt7_measurement_to_json(yajl_gen gen, net_ndt7_measurement_t measurement) {
+    return 0;
+}
+
+int net_ndt7_measurement_from_json(net_ndt7_measurement_t measurement, yajl_val val, error_monitor_t em) {
+    return 0;
+}
+
+
 
