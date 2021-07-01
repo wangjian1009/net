a3d3b2b73326a70d648b3c5b5a3c750be10b0201
Loki <wangjian1009@gmail.com>
2021-07-01 17:36:58 +0800

    mingw build

--- buildtools/cmake/depends-mbedtls.cmake
+++ buildtools/cmake/depends-mbedtls.cmake
@@ -2,6 +2,10 @@ set(mbedtls_base ${CMAKE_CURRENT_LIST_DIR}/../../depends/mbedtls)
 
 file(GLOB mbedtls_source ${mbedtls_base}/library/*.c)
 
+if(WIN32)
+  set(mbedtls_link_libraries Ws2_32)
+endif()
+
 set(mbedtls_compile_definitions
   MBEDTLS_THREADING_PTHREAD
   MBEDTLS_THREADING_C)
@@ -17,3 +21,5 @@ add_library(mbedtls STATIC ${mbedtls_source})
 target_compile_options(mbedtls PRIVATE ${mbedtls_compile_options})
 target_include_directories(mbedtls PUBLIC ${mbedtls_base}/include)
 target_compile_definitions(mbedtls INTERFACE ${mbedtls_compile_definitions})
+
+target_link_libraries(mbedtls INTERFACE ${mbedtls_link_libraries})
